984e6fc0a623a7243c1c00d6d1f4c09e37a26c3e
MchllD <mich11dimafelix@gmail.com>
2024-01-06 22:57:02 +0800

    pseudocode

---
+++ highernumber.py
@@ -0,0 +1,34 @@
+# Ask user to input 3 numbers. Find and print the biggest number using only if-else statement.
+
+# Ask the user to input five numbers
+Input the first number
+Input the second number
+Input the third number
+Input the fourth number
+Input the fifth number
+
+# Check which number is the largest among the five
+if first number is greater than or equal to second number 
+   and first number is greater than or equal to third number 
+   and first number is greater than or equal to fourth number 
+   and first number is greater than or equal to fifth number:
+    Largest number is the first number
+else if second number is greater than or equal to first number 
+        and second number is greater than or equal to third number 
+        and second number is greater than or equal to fourth number 
+        and second number is greater than or equal to fifth number:
+    Largest number is the second number
+else if third number is greater than or equal to first number 
+        and third number is greater than or equal to second number 
+        and third number is greater than or equal to fourth number 
+        and third number is greater than or equal to fifth number:
+    Largest number is the third number
+else if fourth number is greater than or equal to first number 
+        and fourth number is greater than or equal to second number 
+        and fourth number is greater than or equal to third number 
+        and fourth number is greater than or equal to fifth number:
+    Largest number is the fourth number
+else:
+    Largest number is the fifth number
+
+Print the largest number
